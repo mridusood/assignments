a9cf0f6fee6d7abbe62675d115acaf35c9f361dc
mridusood <richasood120@gmail.com>
2018-06-06 19:27:12 +0530

    started assignment

---
+++ venv/file2.py
@@ -0,0 +1,32 @@
+#a2_q1
+
+print("hello world")
+
+#a2_q2
+
+print("acad"+"view")
+
+#a2_q3
+
+x="mridu"
+y="dav uni"
+z=20
+print("hi,my name is %s, i am from %s,my age is %d"%(x,y,z))
+
+#a2_q4
+
+print('let\'s get started')
+
+#a2_q5
+
+s="acadview"
+course="Python"
+fees=5000
+print("thanks for choosing %s, your course is %s, your fees is %d"%(s,course,fees))
+
+#a2_q6
+
+name="Tony Stark"
+salary=1000000
+print("%s %d"%(name,salary))
+
