ef9edb7b1b4e1397f17ec42dac3397d29a38e6b6
mridusood <richasood120@gmail.com>
2018-06-07 12:07:03 +0530

    assignment

---
+++ test.py
@@ -0,0 +1,22 @@
+l2=["happy",[1,2,3]]
+print(l2[1][2])
+l=[1,2,3,4,5]
+l.append(6)
+print(l)
+l.pop(5)
+print(l)
+l.insert(2,"hello")
+print(l)
+l.reverse()
+print(l)
+m=[3,5,1,7,2]
+m.sort()
+print(m)
+fruit=["apple","grapes","melon","apple"]
+print(fruit.count("apple"))
+a=18
+b="hello"
+print(str(a),b)
+print(str(a)+ b)
+a=input("enter no.")
+
