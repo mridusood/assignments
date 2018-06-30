a24e6043956b05131fdcbbd8ea585057f277df70
mridusood <richasood120@gmail.com>
2018-06-30 19:31:23 +0530

    started assignment

--- asgn.py
+++ asgn.py
@@ -2,27 +2,33 @@ import numpy as np
 import math
 
 #Ques1
-x=np.random.random((10,1))
-print (x)
-m=x.mean()
+a=np.random.random((10,1))
+print (a)
+m=a.mean()
 print("mean is "+str(m))
 
 #Ques2
-y=np.random.random((20,1))
-print(y)
-a=y.var()
-print("variance is "+str(a))
-b=y.std()
-print("standard deviaation is "+str(b))
+b=np.random.random((20,1))
+print(b)
+v=b.var()
+print("variance is "+str(v))
+s=b.std()
+print("standard deviaation is "+str(s))
 
 #Ques3
-s=np.random.random((10,20))
-t=np.random.random((20,25))
-#a.dtype=np.float64
-#b.dtype=np.float64
-#print(a*b)
-c=s@t
+a=np.random.random((10,20))
+b=np.random.random((20,25))
+
+c=np.matmul(a,b)
 print(c)
 d=np.sum(c)
 print("sum of all elements of new matrix is "+str(d))
-#ques4
+
+#Ques4
+a=np.random.random((10,1))
+print(a)
+b=[]
+for i in range (0,10):
+    x=1/(1+math.exp(-(a[i,0])))
+    b.append(x)
+print(b)
---
+++ panda.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+#Q1
+
+data={'name':['mridu'],'age':[20],'mail id':['richasood120@gmail.com@gmail.com'],'phone no.':[879722410]}
+df=pd.DataFrame(data)
+print(df)
+print("\n \n after adding information of my friend ")
+df.loc[1]=['abc',22,'abc98@gmail.com',7647864782]
+print(df)
+
+
+#Q2
+data=pd.read_csv("data.csv")
+df=pd.DataFrame(data)
+print("first five rows of data frame are")
+print(df.head(5))
+print("first ten rows of data frame are")
+print(df.head(10))
+print("basic statics are")
+print("axes\n"+str(df.axes))
+print("datatypes\n"+str(df.dtypes))
+print("dimension\n"+str(df.ndim))
+print("shape\n"+str(df.shape))
+print("size\n"+str(df.size))
+print("last 5 rows are")
+print(df.tail(5))
+
+x=df.loc[:,'Location']
+print(x)
+print("basic statics are")
+print("axes\n"+str(x.axes))
+print("datatypes\n"+str(x.dtypes))
+print("dimension\n"+str(x.ndim))
+print("shape\n"+str(x.shape))
+print("size\n"+str(x.size))
